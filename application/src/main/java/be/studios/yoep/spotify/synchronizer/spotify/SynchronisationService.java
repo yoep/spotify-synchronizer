9b17c67ddd0b91519c8cc7acede613f0db43a91f
Yoeri <yoeri.peeters10@telenet.be>
2018-03-11 19:49:30 +0100

    Added feedback for total tracks

--- application/src/main/java/be/studios/yoep/spotify/synchronizer/spotify/SynchronisationService.java
+++ application/src/main/java/be/studios/yoep/spotify/synchronizer/spotify/SynchronisationService.java
@@ -3,8 +3,10 @@ package be.studios.yoep.spotify.synchronizer.spotify;
 import be.studios.yoep.spotify.synchronizer.authorization.AuthorizationService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Data
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class SynchronisationService {
 
     public boolean startSynchronisation() {
         totalTracks = spotifyService.getTotalTracks();
+        log.debug("Synchronizing " + totalTracks + " tracks");
         return true;
     }
 }
